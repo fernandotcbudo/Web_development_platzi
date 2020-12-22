163b6797204d5d32c62c3e6761c8f00e63598820
Fernando TC <g561.p@hotmail.com>
2020-12-22 15:54:46 -0500

    Final example

--- program_basic/canvas/draw_final.js
+++ program_basic/canvas/draw_final.js
@@ -1,10 +1,20 @@
 var d = document.getElementById("draw_1");
 var canvas_1 = d.getContext("2d");
 
-canvas_1.beginPath();
-canvas_1.strokeStyle = "red";
-canvas_1.moveTo(50,100);
-canvas_1.lineTo(50,200);
-canvas_1.stroke();
-canvas_1.closePath();
+function draw_line(color,x_ini,y_ini,x_fin,y_fin)
+{
+    canvas_1.beginPath();
+    canvas_1.strokeStyle = color;
+    canvas_1.moveTo(x_ini,y_ini);
+    canvas_1.lineTo(x_fin,y_fin);
+    canvas_1.stroke();
+    canvas_1.closePath();
 
+}
+
+draw_line("red",100,100,200,100);
+draw_line("blue",100,100,100,200);
+draw_line("yellow",200,100,200,200);
+draw_line("green",100,200,200,200);
+draw_line("pink",150,50,250,50);
+draw_line("brown",100,100,150,50);
