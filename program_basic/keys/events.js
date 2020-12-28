02ad13555882d589b8d62d9a387454321672541a
Fernando TC <g561.p@hotmail.com>
2020-12-28 09:23:38 -0500

    Final modifications

--- program_basic/keys/events.js
+++ program_basic/keys/events.js
@@ -1,30 +1,54 @@
-document.addEventListener("keyup",draw_Key);
-
 var keys = {
     UP:38,
     DOWN:40,
-    LEFT:37,
+    LEFT:37,    
     RIGHT:39
 };
 
-console.log(keys);
+document.addEventListener("keyup",draw_Key);
+var listener= document.getElementById("canvas_ev")
+var paper= listener.getContext("2d");
+var x= 150;
+var y= 150;
+
+function draw_line(color,x_ini,y_ini,x_fin,y_fin,canvas_1)
+{
+    canvas_1.beginPath();
+    canvas_1.strokeStyle = color;
+    canvas_1.lineWidth= 3;
+    canvas_1.moveTo(x_ini,y_ini);
+    canvas_1.lineTo(x_fin,y_fin);
+    canvas_1.stroke();
+    canvas_1.closePath();
+
+}
+
+draw_line("red",149,149,151,151,paper);
+
 
 function draw_Key(event)
-{
+{   
+    var color_f="red"
+    var movement= 10
+
     if(event.keyCode == keys.UP)
     {
-        console.log("arriba");
+        draw_line(color_f,x,y,x,y - movement, paper);
+        y = y - movement;
     }
     if(event.keyCode == keys.DOWN)
     {
-        console.log("abajo");
+        draw_line(color_f,x,y,x,y + movement, paper);
+        y = y + movement;
     }
     if(event.keyCode == keys.LEFT)
     {
-        console.log("izquierda");
+        draw_line(color_f,x,y,x - movement,y, paper);
+        x = x - movement;
     }
     if(event.keyCode == keys.RIGHT)
     {
-        console.log("derecha");
+        draw_line(color_f,x,y,x + movement,y, paper);
+        x = x + movement;
     }
 }
