4e71f695a2d933169cde57a100d4a52ed58e9993
Fernando TC <g561.p@hotmail.com>
2020-12-29 13:36:01 -0500

    Charging images

--- program_basic/villa_platzi/platzi.js
+++ program_basic/villa_platzi/platzi.js
@@ -1,16 +1,47 @@
-z= aleatory(10,20);
+var vp= document.getElementById("Villaplatzi");
+var paper= vp.getContext("2d");
+var map= "tile.png";
 
-for(var i=0;i<10;i++)
+
+var imagef= new Image();
+imagef.src= map;
+imagef.addEventListener("load",drawing);
+
+var imagec= new Image();
+imagec.src= "vaca.png";
+imagec.addEventListener("load",draw_cow);
+
+var imagech= new Image();
+imagech.src= "pollo.png";
+imagech.addEventListener("load",draw_ch);
+
+var imagep= new Image();
+imagep.src= "cerdo.png";
+imagep.addEventListener("load",draw_p);
+
+function drawing()
 {
-    z= aleatory(10,20);
-    document.write(z + ",");
+    paper.drawImage(imagef,0,0);
 }
 
-document.write(z);
+function draw_cow()
+{
+    paper.drawImage(imagec,10,10);  
+}
+
+function draw_ch()
+{
+    paper.drawImage(imagech,10,200);
+}
+
+function draw_p()
+{
+    paper.drawImage(imagep,10,150);
+}
 
 function aleatory(min,max)
 {
     var result;
     result=  Math.floor((Math.random() * (max-min + 1)) + min);
     return result;
-}
+}   
