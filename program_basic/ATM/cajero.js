328094633f670bf37695a598e4e40be4e7253ed8
Fernando TC <g561.p@hotmail.com>
2021-01-16 15:06:02 -0500

    Final ATM

--- program_basic/ATM/cajero.js
+++ program_basic/ATM/cajero.js
@@ -7,10 +7,12 @@ class Bill
     }
 }
 
+
 function delivMoney()
 {   
     var t= document.getElementById("bills");
-    money= parseInt(t.valor);
+    money= parseInt(t.value);
+
     for(var bi of box)
     {
         if(money > 0)
@@ -30,21 +32,34 @@ function delivMoney()
         }
     }
     if (money > 0)
-    {
-        console.log("x");
+    {   
+        r.innerHTML= "I don't have this amount of money";
     }
-    console.log(delivered);
+    else
+    {
+        for(var e of delivered)
+        {   
+            if (e.cantidad > 0)
+            {r.innerHTML= r.innerHTML + e.cantidad + " bills of $" + e.valor + "<br />";}
+        } 
+    }
 }
+
+var box= [];
+box.push(new Bill(100,5));
+box.push(new Bill(50,10));
+box.push(new Bill(20,5));
+box.push(new Bill(10,10));
+box.push(new Bill(5,5));
+
+var delivered=[];
 
 var div= 0;
 var paper= 0;
-var money =0;
-var box= [];
-var delivered=[];
-box.push(new Bill(50,10));
-box.push(new Bill(20,5));
-box.push(new Bill(10,5));
+var money= 0;
 
 var b= document.getElementById("ex");
+var r= document.getElementById("result");
 b.addEventListener("click",delivMoney);
+
 
