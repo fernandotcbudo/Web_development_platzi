e76d3e547c401e7c68a62f468ce1f2903bd1b0b8
Fernando TC <g561.p@hotmail.com>
2021-01-26 11:27:00 -0500

    Try-except function

--- Python sin fronteras/excercise_2.py
+++ Python sin fronteras/excercise_2.py
@@ -2,6 +2,7 @@
 
 def main():
     welcome= int(input('Please type 1 if you want to sum or 2 if you want to substract: '))
+
     if welcome == 1:
         Sum_calculator()
     elif welcome == 2:
@@ -10,15 +11,51 @@ def main():
         print('Is not an option')
 
 def Sum_calculator():
-    firstNum= int(input('Write the first number: '))
-    secondNum= int(input('Write the second number: '))
-    result= print(f'The result is {firstNum+secondNum}')
+    firstNum= input('Write the first number: ')
+    secondNum= input('Write the second number: ')
+    result=''
+    try:
+        firstNum= int(firstNum)
+
+    except:
+        firstNum= 'Is not a number'
+
+    try:
+        secondNum= int(secondNum)
+
+    except:
+        secondNum= 'Is not a number'
+    
+    if firstNum == 'Is not a number' or secondNum == 'Is not a number':
+        print('Please write a number, try again')
+    else:
+        result= print(f'The result is {firstNum+secondNum}')
+
     return result
 
+ 
+
 def Subs_calculator():
-    firstNum= int(input('Write the first number: '))
-    secondNum= int(input('Write the second number: '))
-    result= print(f'The result is {firstNum-secondNum}')
+    firstNum= input('Write the first number: ')
+    secondNum= input('Write the second number: ')
+    result=''
+    try:
+        firstNum= int(firstNum)
+
+    except:
+        firstNum= 'Is not a number'
+
+    try:
+        secondNum= int(secondNum)
+
+    except:
+        secondNum= 'Is not a number'
+    
+    if firstNum == 'Is not a number' or secondNum == 'Is not a number':
+        print('Please write a number, try again')
+    else:
+        result= print(f'The result is {firstNum-secondNum}')
+
     return result
 
 if __name__ == "__main__":
