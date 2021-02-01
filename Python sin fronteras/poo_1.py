af6c98dbef1c3beef2979d5bd8576bb2a77afe14
Fernando TC <g561.p@hotmail.com>
2021-02-01 14:53:37 -0500

    Methods examples

--- Python sin fronteras/poo_1.py
+++ Python sin fronteras/poo_1.py
@@ -1,14 +1,22 @@
 class User:
-   def __init__(self,name,lastname):
-    self.name= name
-    self.lastname= lastname
+    def __init__(self,name,lastname):
+        self.name= name
+        self.lastname= lastname
+
+    def hello(self):
+       print(f'Welcome {self.name} {self.lastname}')
+       return self.name,self.lastname
+
 
 if __name__ == '__main__':
     inputUser1= input('What is yout name? ')
     inputUser2= input('What is yout last name? ')
     user=User(inputUser1,inputUser2)
+    user_h= user.hello()
 
-    print(user.name,user.lastname)
+    
+
+
 
     
 
