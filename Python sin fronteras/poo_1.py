4adc436a266cea27e58f675fcf1caea5cf8ed3e8
Fernando TC <g561.p@hotmail.com>
2021-02-01 22:43:30 -0500

    Modifying methods

--- Python sin fronteras/poo_1.py
+++ Python sin fronteras/poo_1.py
@@ -1,17 +1,20 @@
 class User:
-    def __init__(self,name,lastname):
+    def __init__(self,name,lastname,age):
         self.name= name
         self.lastname= lastname
+        self.age= age
 
     def hello(self):
-       print(f'Welcome {self.name} {self.lastname}')
+       print(f'Welcome {self.name} {self.lastname} , your age is {self.age}')
        return self.name,self.lastname
 
 
 if __name__ == '__main__':
     inputUser1= input('What is yout name? ')
     inputUser2= input('What is yout last name? ')
-    user=User(inputUser1,inputUser2)
+    ageFinal= 20
+    user=User(inputUser1,inputUser2,ageFinal)
+    user.age= 30
     user_h= user.hello()
 
     
