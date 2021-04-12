fb19eebe2f5190deb74da70c907507e7d740ddd0
Fernando TC <g561.p@hotmail.com>
2021-04-11 21:16:08 -0500

    Http methods

--- Python sin fronteras/python/app/hello.py
+++ Python sin fronteras/python/app/hello.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 app= Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Helloooooo world'
 
-@app.route('/menu1')
+@app.route('/menu1',methods=['GET','POST'])
 def menu1():
     return 'Menu 1'
 @app.route('/menu2')
