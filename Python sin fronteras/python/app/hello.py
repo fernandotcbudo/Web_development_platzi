3cff415144742d5ba3c5676d11d3b668722e3df0
Fernando TC <g561.p@hotmail.com>
2021-04-12 13:31:36 -0500

    Urls in flask

--- Python sin fronteras/python/app/hello.py
+++ Python sin fronteras/python/app/hello.py
@@ -1,14 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, url_for
 app= Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Helloooooo world'
 
-@app.route('/menu1',methods=['GET','POST'])
-def menu1():
-    return 'Menu 1'
+@app.route('/menu1/<post1>',methods=['GET','POST'])
+def menu1(post1):
+    if request.method == 'GET':
+            return 'El id del post es: ' + post1
+    else:
+        return 'Este es otro metodo '
+
 @app.route('/menu2', methods=['POST'])
 def menu2():
+    print(url_for('index',post1=2))
     print(request.form)
     return 'Menu 2'
