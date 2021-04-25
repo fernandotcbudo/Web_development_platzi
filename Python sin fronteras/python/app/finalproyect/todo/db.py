5979f12da0e3269b03c31bb51f715ec74087bcfe
Fernando TC <g561.p@hotmail.com>
2021-04-25 12:02:42 -0500

    Script for initializing db

--- Python sin fronteras/python/app/finalproyect/todo/db.py
+++ Python sin fronteras/python/app/finalproyect/todo/db.py
@@ -20,5 +20,17 @@ def close_db(e=None):
     if db is not None:
         db.close()
 
+def init_db():
+    db,c = get_db()
+    for i in instructions:
+        c.execute(i)
+    db.commit()
+@click.command('init-db')
+@with_appcontext
+
+def init_db_command():
+    init_db()
+    click.echo('Base de datos inicializada')
+
 def init_app(app):
     app.teardown_appcontext(close_db)
---
+++ Python sin fronteras/python/app/finalproyect/todo/schema.py
@@ -0,0 +1,6 @@
+instructions= [
+    'SET FOREING_KEY_CHECKS=0;' 
+    'DROP TABLE IF EXISTS todo;'
+    'DROP TABLE IF EXISTS user;'
+    'SET FOREING_KEY_CHECKS=1;' 
+ ]
