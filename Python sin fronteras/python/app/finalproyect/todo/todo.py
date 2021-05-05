4440008863076bb1d3ca3bf48cd72fdf84c8c179
Fernando TC <g561.p@hotmail.com>
2021-05-05 13:08:38 -0500

    Protecting routes-creating todo list

--- Python sin fronteras/python/app/finalproyect/todo/todo.py
+++ Python sin fronteras/python/app/finalproyect/todo/todo.py
@@ -1,4 +1,4 @@
- from flask import (
+from flask import (
     Blueprint, flash, g, render_template, request, url_for, session, redirect
 )
 
@@ -6,4 +6,15 @@ from werkzeug.exceptions import abort
 from todo.auth import login_required
 from todo.db import get_db
 
-bp= Blueprint('todo' __name__)
+bp= Blueprint('todo' , __name__)
+
+@bp.route('/')
+@login_required
+def index():
+    db, c = get_db()
+    c.execute(
+        'select i.id, t.description, u.username, t.completed, t.created_at from todo t JOIN useru on t.created_by= u.id order by created_at desc'
+    )
+    todos= c.fetchall()
+    
+    return render_template('todo/index.html', todos=todos)
