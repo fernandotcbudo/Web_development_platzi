97fa09cbf3df82d2808822908175298a17dae1df
FernandoTC <g561.p@hotmail.com>
2022-09-30 15:42:45 -0500

    Changes final

---
+++ Fundamentos_typescript/src/04-numbers.ts
@@ -0,0 +1,13 @@
+(() => {
+  let productPrice= 100;
+  productPrice = 110;
+  productPrice = productPrice + 1;
+  console.log('Price:',productPrice);
+
+  let productStore: number;
+
+  let discount = parseInt('123');
+  if (discount > 100){
+    console.log('It is bigger');
+  }
+}) ();
